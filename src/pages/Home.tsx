f46c9cf23e7eaee33bd5162001b306c41be98340
Osman Yakub Beni <osmanbeni2001@gmail.com>
2026-02-17 13:34:30 +0000

    fix bug

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,22 @@
 import type { FC } from "hono/jsx";
 
 export const Home: FC = (props) => {
+  const delegated = props.regionSelected
+    ? {}
+    : { "hx-trigger": "revealed", "hx-get": "/countries?page=1" };
   return (
     <div>
-      {/* <section
-        class="countries"
-        hx-get="/countries"
-        hx-trigger="revealed"
-      ></section> */}
       <section
         class="home-grid"
-        hx-get="/countries?page=1"
-        hx-trigger="revealed"
         hx-swap="beforeend"
         id="countries-container"
-      ></section>
-      {props.childen} <div class="btn-container" id="button-container"></div>
+        {...delegated}
+      >
+        {/* FIX: Put the children INSIDE the container so they show up on refresh */}
+        {props.children}
+      </section>
+
+      <div class="btn-container" id="button-container"></div>
     </div>
   );
 };
