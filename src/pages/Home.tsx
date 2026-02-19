c75b9264d10148e7a4c690a092e039ac718779f6
Osman Yakub Beni <osmanbeni2001@gmail.com>
2026-02-19 17:31:51 +0000

    fix: region filter countries display

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,6 +12,7 @@ export const Home: FC = (props) => {
     <div>
       <section class="home-grid" id="countries-container" {...delegated}>
         {/* FIX: Put the children INSIDE the container so they show up on refresh */}
+        {props.children}
       </section>
 
       <div class="btn-container" id="button-container"></div>
